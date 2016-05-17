b655d843f73edb1f4fb3c4e017565ddd835cc82d
liangxuan <476002526@qq.com>
2016-05-17 15:57:55 +0800

    fix bug

--- DRAW_load_batch.py
+++ DRAW_load_batch.py
@@ -19,7 +19,8 @@ def loadimg(srcdir, names, w=54, h=54, p=0.1):
 	
 	for i in range(n):
 		image = names[i]
-		img = srcdir + image.strip("\n")
+		image = image.strip("\n")
+		img = srcdir + image
 		print img
 		rd = cv2.imread(img)
 		print(rd)
