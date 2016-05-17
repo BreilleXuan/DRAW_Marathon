b1d1c936c3467fb5ea37ac5257acf56aedd4005e
liangxuan <476002526@qq.com>
2016-05-17 17:17:23 +0800

    fix bug

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
-from DRAW_parameters import *
-from DRAW_load_batch import *
+import cv2
 import numpy as np
-
-imglist = load_name_list(img_name_file)
-a = iterate_minibatches(imglist, batch_size, shuffle=True)
+from File.file_csv import *
 
 
+namelist = load_name_list("data/namefile.csv")
+srcdir = "data/cutted_images/"
+img_name = iterate_minibatches(namelist, 4, shuffle=True)
 
+name_batch = img_name[0]
+print(name_batch)
 
-
-
-
-
+loadimg(srcdir, name_batch)
