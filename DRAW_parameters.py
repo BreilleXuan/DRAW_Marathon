231e1fab6add973513e0134654e39a483f3df320
liangxuan <476002526@qq.com>
2016-05-18 17:24:01 +0800

    adjust parameters

--- DRAW_parameters.py
+++ DRAW_parameters.py
@@ -10,11 +10,11 @@ FLAGS = tf.flags.FLAGS
 A,B = 54,54 # image width,height
 img_size = B*A*3 # the canvas size
 
-enc_size = 100 # number of hidden units / output size in LSTM
-dec_size = 100
+enc_size = 800 # number of hidden units / output size in LSTM
+dec_size = 800
 
-read_n = 2 # read glimpse grid width/height
-write_n = 2 # write glimpse grid width/height
+read_n = 12 # read glimpse grid width/height
+write_n = 12 # write glimpse grid width/height
 read_size = 2*read_n*read_n*3 if FLAGS.read_attn else 2*img_size
 write_size = write_n*write_n*3 if FLAGS.write_attn else img_size
 z_size=100 # QSampler output size
@@ -26,7 +26,7 @@ jitter = 0.1
 
 batch_size=8 # training minibatch size
 train_iters=10000
-learning_rate=3e-3 # learning rate for optimizer
+learning_rate=1e-2 # learning rate for optimizer
 eps=1e-10 # epsilon for numerical stability
 ld = 1.
 
